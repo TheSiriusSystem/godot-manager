1244c139605a619ff3ce11cfb00796a87c9831af
Roopkotha Studio <pikachuking54321@gmail.com>
2022-12-12 22:16:42 +0600

    Show any errors in the app and remove forced waits

--- components/Panels/SplashScreen.cs
+++ components/Panels/SplashScreen.cs
@@ -1,10 +1,10 @@
 using Godot;
-using System;
 using Godot.Sharp.Extras;
 
 public class SplashScreen : Control
 {
 	[NodePath] private Label VersionInfo = null;
+	[NodePath] private Label LoadingLabel = null;
 	private Thread _thread;
 
 	// Called when the node enters the scene tree for the first time.
@@ -12,11 +12,10 @@ public class SplashScreen : Control
 	{
 		this.OnReady();
 		VersionInfo.Text = $"Version {VERSION.GodotManager}";
-		var timer = GetTree().CreateTimer(1.0f);
-		timer.Connect("timeout", this, "OnTimeout_LoadResources");
+		CallDeferred("GDThread_Init");
 	}
 
-	void OnTimeout_LoadResources()
+	void GDThread_Init()
 	{
 		_thread = new Thread();
 		_thread.Start(this, "GDThread_Loader");
@@ -25,12 +24,14 @@ public class SplashScreen : Control
 	void GDThread_Loader()
 	{
 		var loader = ResourceLoader.LoadInteractive("res://Scenes/SceneManager.tscn");
-		GdAssert.Assert(loader != null, "Loader failed to start loading SceneManager");
-		if (loader is null) return;
+		if (loader == null)
+		{
+			LoadingLabel.Text = "Failed to load 'Scenes/SceneManager.tscn'!";
+			return;
+		}
 
 		do
 		{
-			OS.DelayMsec(100);
 			var err = loader.Poll();
 			if (err == Error.FileEof)
 			{
@@ -39,7 +40,7 @@ public class SplashScreen : Control
 				break;
 			} else if (err != Error.Ok)
 			{
-				GD.PrintErr("There was an error loading.");
+				LoadingLabel.Text = "An error occurred.\nError Code: " + err.ToString();
 				break;
 			}
 		} while (true);
