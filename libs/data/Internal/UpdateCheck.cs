08d653423414aaa0ed54c605685cb0ebc73b90a2
TheSiriusSystem <91137432+TheSiriusSystem@users.noreply.github.com>
2023-10-04 06:36:13 +0600

    Delete UpdateCheck.cs

--- libs/data/Internal/UpdateCheck.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using Newtonsoft.Json;
-using DateTime = System.DateTime;
-
-[JsonObject(MemberSerialization.OptIn)]
-public class UpdateCheck : Object
-{
-	[JsonProperty] public DateTime LastCheck { get; set; }
-}
